e0de8da1a543ae1cfb975b22eb07b37f5a621631
Streamtg <202310475@issu.edu.do>
2025-06-27 23:46:14 -0400

    Update reader.go

--- internal/utils/reader.go
+++ internal/utils/reader.go
@@ -1,142 +1,53 @@
-package utils
+package main
 
 import (
 	"context"
-	"fmt"
 	"io"
+	"net/http"
+	"strconv"
 
 	"github.com/celestix/gotgproto"
 	"github.com/gotd/td/tg"
 	"go.uber.org/zap"
+
+	"github.com/celestix/gotgproto/utils" // Ruta original de telegramReader
 )
 
-type telegramReader struct {
-	ctx           context.Context
-	log           *zap.Logger
-	client        *gotgproto.Client
-	location      *tg.InputDocumentFileLocation
-	start         int64
-	end           int64
-	next          func() ([]byte, error)
-	buffer        []byte
-	bytesread     int64
-	chunkSize     int64
-	i             int64
-	contentLength int64
-}
+var logger *zap.Logger
 
-func (*telegramReader) Close() error {
-	return nil
-}
+func main() {
+	logger, _ = zap.NewDevelopment()
+	defer logger.Sync()
 
-func NewTelegramReader(
-	ctx context.Context,
-	client *gotgproto.Client,
-	location *tg.InputDocumentFileLocation,
-	start int64,
-	end int64,
-	contentLength int64,
-) (io.ReadCloser, error) {
+	// Inicializa el cliente de Telegram
+	client := gotgproto.NewClient() // Aquí deberías conectar y autenticar tu cliente antes de usarlo
 
-	r := &telegramReader{
-		ctx:           ctx,
-		log:           Logger.Named("telegramReader"),
-		location:      location,
-		client:        client,
-		start:         start,
-		end:           end,
-		chunkSize:     int64(1024 * 1024),
-		contentLength: contentLength,
-	}
-	r.log.Sugar().Debug("Start")
-	r.next = r.partStream()
-	return r, nil
-}
+	http.HandleFunc("/video", func(w http.ResponseWriter, r *http.Request) {
+		ctx := context.Background()
 
-func (r *telegramReader) Read(p []byte) (n int, err error) {
+		// TODO: Inicializa correctamente estos valores desde tu lógica
+		var location tg.InputFileLocationClass = /* ubicación del archivo en Telegram */ nil
+		start := int64(0)
+		end := int64(1024 * 1024 * 100) // por ejemplo, 100 MB
+		contentLength := end - start
 
-	if r.bytesread == r.contentLength {
-		r.log.Sugar().Debug("EOF (bytesread == contentLength)")
-		return 0, io.EOF
-	}
-
-	if r.i >= int64(len(r.buffer)) {
-		r.buffer, err = r.next()
-		r.log.Debug("Next Buffer", zap.Int64("len", int64(len(r.buffer))))
+		reader, err := utils.NewTelegramReader(ctx, client, location, start, end, contentLength)
 		if err != nil {
-			return 0, err
+			http.Error(w, "failed to create reader", http.StatusInternalServerError)
+			return
 		}
-		if len(r.buffer) == 0 {
-			r.next = r.partStream()
-			r.buffer, err = r.next()
-			if err != nil {
-				return 0, err
-			}
+		defer reader.Close()
 
-		}
-		r.i = 0
-	}
-	n = copy(p, r.buffer[r.i:])
-	r.i += int64(n)
-	r.bytesread += int64(n)
-	return n, nil
-}
+		w.Header().Set("Content-Type", "video/mp4")
+		w.Header().Set("Content-Length", strconv.FormatInt(contentLength, 10))
+		w.WriteHeader(http.StatusOK)
+		_, _ = io.Copy(w, reader)
+	})
 
-func (r *telegramReader) chunk(offset int64, limit int64) ([]byte, error) {
+	http.HandleFunc("/", func(w http.ResponseWriter, r *http.Request) {
+		http.ServeFile(w, r, "static/index.html")
+	})
 
-	req := &tg.UploadGetFileRequest{
-		Offset:   offset,
-		Limit:    int(limit),
-		Location: r.location,
-	}
-
-	res, err := r.client.API().UploadGetFile(r.ctx, req)
-
-	if err != nil {
-		return nil, err
-	}
-
-	switch result := res.(type) {
-	case *tg.UploadFile:
-		return result.Bytes, nil
-	default:
-		return nil, fmt.Errorf("unexpected type %T", r)
-	}
-}
-
-func (r *telegramReader) partStream() func() ([]byte, error) {
-
-	start := r.start
-	end := r.end
-	offset := start - (start % r.chunkSize)
-
-	firstPartCut := start - offset
-	lastPartCut := (end % r.chunkSize) + 1
-	partCount := int((end - offset + r.chunkSize) / r.chunkSize)
-	currentPart := 1
-
-	readData := func() ([]byte, error) {
-		if currentPart > partCount {
-			return make([]byte, 0), nil
-		}
-		res, err := r.chunk(offset, r.chunkSize)
-		if err != nil {
-			return nil, err
-		}
-		if len(res) == 0 {
-			return res, nil
-		} else if partCount == 1 {
-			res = res[firstPartCut:lastPartCut]
-		} else if currentPart == 1 {
-			res = res[firstPartCut:]
-		} else if currentPart == partCount {
-			res = res[:lastPartCut]
-		}
-
-		currentPart++
-		offset += r.chunkSize
-		r.log.Sugar().Debugf("Part %d/%d", currentPart, partCount)
-		return res, nil
-	}
-	return readData
+	logger.Sugar().Info("Server started at http://localhost:8080")
+	http.ListenAndServe(":8080", nil)
 }
